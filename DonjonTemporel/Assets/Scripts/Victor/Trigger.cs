29a978a0d36c93053feb23f453dc0725148ef7ee
victorgileau <vgileau@hotmail.com>
2024-11-18 21:18:02 -0500

    modification-vitesse-joueur+test-trigger.cs

---
+++ DonjonTemporel/Assets/Scripts/Victor/Trigger.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Trigger : MonoBehaviour
+{
+    [SerializeField] UnityEvent onTriggerEnter;
+
+    [SerializeField] UnityEvent onTriggerExit;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        onTriggerEnter.Invoke();
+        Debug.Log("enter");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        onTriggerExit.Invoke();
+        Debug.Log("leave");
+    }
+}
