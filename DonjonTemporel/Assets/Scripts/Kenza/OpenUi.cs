77633a5ce380d376c5d5c4cac1bcf8f2d21d11b1
IanQc <ian.corbin.qc@gmail.com>
2024-12-03 18:04:53 -0500

    ascenceur_marche_pus

--- DonjonTemporel/Assets/Scripts/Kenza/OpenUi.cs
+++ DonjonTemporel/Assets/Scripts/Kenza/OpenUi.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Presets;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class OpenUi : MonoBehaviour
 {
