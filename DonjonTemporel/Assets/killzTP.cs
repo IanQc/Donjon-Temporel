2bc56529ee64a3875c2b339ef7e90b28b72f4046
IanQc <ian.corbin.qc@gmail.com>
2024-12-10 18:35:26 -0500

    killzone

---
+++ DonjonTemporel/Assets/killzTP.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Killzone : MonoBehaviour
+{
+    public Transform teleportDestination; // La destination où le joueur sera téléporté
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Vérifier si l'objet entrant est le joueur
+        if (other.CompareTag("DetectP"))
+        {
+            // Déplacer le joueur à la destination de téléportation
+            other.transform.position = teleportDestination.position;
+
+            // Optionnel : Réinitialiser la rotation du joueur
+            other.transform.rotation = teleportDestination.rotation;
+        }
+    }
+}
